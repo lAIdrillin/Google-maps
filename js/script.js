adc033d00103d2c459b467f26602fa852abf1458
lAIdrillin <gabrielgcastro1809@gmail.com>
2025-05-20 17:33:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 const locations = {
     madrid: [
-        { lat:40.456405086658194,  lng: -3.688907896855511, tittle: "Bernabeu"},
-        { lat:40.42061406254445,  lng:-3.6890222666912194 , tittle: "Puerta de Alcala"},
+        { lat:40.45310696447501, lng:-3.6880131863036696, tittle: "Bernabeu"},
+        { lat:40.420125098014, lng:-3.688705230915027, tittle: "Puerta de Alcala"},
         { lat:40.491089078668125,   lng: -3.5647155135021222, tittle: "Aeropuerto de Barajas"}
     ],
     paris: [
-        { lat:48.8534, lng: 2.3488, tittle: "Torre Eiffel"},
-        { lat:48.87499217828519,  lng: 2.2959531659624326, tittle: "Arco del triunfo"},
-        { lat:48.85402852048143,  lng: 2.3502389585497423, tittle: "Nottre Dame"}
+        { lat:48.85841025645972, lng:2.2944923078862978, tittle: "Torre Eiffel"},
+        { lat:48.873896592117916, lng:2.29499966203389, tittle: "Arco del triunfo"},
+        { lat:48.85302032202513, lng:2.349873990389982, tittle: "Nottre Dame"}
     ],
     roma: [
-        {lat:41.90225999743756, lng:12.482865576406345, tittle: "Fontana de Trevi"},
-        {lat:41.8921407223458, lng:12.492090678304947, tittle: "Coliseo"},
-        {lat:41.89327876491798, lng:12.486571912390653, tittle: "Foro Romano"}
+        {lat:41.9009841224931, lng:12.48329946954169, tittle: "Fontana de Trevi"},
+        {lat:41.89027997948962, lng:12.492212550815092, tittle: "Coliseo"},
+        {lat:41.89211998049781, lng:12.486435780698526, tittle: "Foro Romano"}
     ],
     viena: [
         { lat:48.2082, lng: 16.3738, tittle: "Palacio de Hofburg"},
