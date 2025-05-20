6d6d5cc68220c41c21f39af675b1bafa67fe635d
Hugovg2791 <hugovila2710@gmail.com>
2025-05-20 16:17:22 +0200

    das

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
-function initMap() {
+const enviar = document.getElementById('enviar');
+const ciudad = document.getElementById('ciudad').value;
+const formulario = document.getElementById('formulario');
+const mapa = document.getElementById('map');
+formulario.style.display = 'block';
+
+function initMap(lat, lng) {
+    mapa.style.display = 'block';
     let map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 40, lng: -4 },
+      center: { lat: lat, lng: lng },
       zoom: 6,
     });
   }
+
+
+
+
+enviar.addEventListener('click', function() {
+
+    let lat;
+    let lng;
+    if (ciudad === 'madrid'){
+        lat = 40.41895303835641
+        lng = -3.7173449515944736
+        initMap(lat, lng);
+    }else if(ciudad === 'paris'){
+        lat = 48.85879187839086
+        lng = 2.352870623663526
+        initMap(lat, lng);
+    }else if(ciudad === 'roma'){
+        lat = 41.89711601940518
+        lng = 12.490647360010009
+        initMap(lat, lng);
+    }else if (ciudad === 'sarajevo'){
+        lat = 43.85627799383089
+        lng =  8.41245180743923
+        initMap(lat, lng);
+    }else if(ciudad === 'viena'){
+        lat = 48.21269348527829
+        lng =  16.36794393076543
+        initMap(lat, lng);
+    }
+    formulario.style.display = 'none';
+    
+});
