ed67dc7e270a4aaffe9540108495966a891926ff
Apocaliptico70 <thebufjunior@gmail.com>
2025-05-20 15:42:00 +0200

    vinculacion de archivos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function initMap(){
+function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 40, lng: -4 },
-        zoom: 6.5,
+      center: { lat: 40, lng: -4 },
+      zoom: 6,
     });
-}
+  }
